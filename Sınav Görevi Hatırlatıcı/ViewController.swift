8266e2a9dd1af1df1fb38fb570d8a9d600f80476
Furkan Dag <furkan@Furkan-MacBook-Pro.local>
2017-01-09 15:26:00 +0300

    Initial Commit

---
+++ Sınav Görevi Hatırlatıcı/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sınav Görevi Hatırlatıcı
+//
+//  Created by Furkan Selcuk Dag on 9.01.2017.
+//  Copyright © 2017 Furkan Selcuk Dag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
